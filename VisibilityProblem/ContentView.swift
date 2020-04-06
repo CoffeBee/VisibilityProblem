f650fbcfe4fe1555554c96ff1c4ee096b7d37c30
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2020-04-06 17:37:57 +0300

    Initial Commit

---
+++ VisibilityProblem/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  VisibilityProblem
+//
+//  Created by Podvorniy Ivan on 06.04.2020.
+//  Copyright © 2020 Podvorniy Ivan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
